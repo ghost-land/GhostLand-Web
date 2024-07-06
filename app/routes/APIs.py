cf4617bfc72115acc0e3c9be6000900e4525ef76
Lenoch <81lennoch@gmail.com>
2024-07-07 00:25:29 +0200

    Re-add APIs for ghosteshop services
    - Restored APIs:
      - `ghosteshop-ds.eshop`
      - `ghosteshop-ds.json`
      - `ghosteshop.eshop`
      - `ghosteshop.json`

--- app/routes/APIs.py
+++ app/routes/APIs.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
+import json
 from run import settings
 
 APIs_bp = Blueprint('APIs', __name__)
@@ -8,3 +9,12 @@ APIs_bp = Blueprint('APIs', __name__)
 @APIs_bp.route('/APIs')
 def index():
     return render_template('APIs.html')
+
+@APIs_bp.route('/<api_file>')
+@APIs_bp.route('/<api_file>/')
+def rss(api_file):
+    if api_file.lower() in settings['api'].get('data-files', []):
+        with open(f'data/{api_file.lower()}', 'r', encoding='utf-8') as f:
+            return json.load(f)
+    
+    abort(404)
