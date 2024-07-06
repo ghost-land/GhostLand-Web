b7d0103afa51d91d0283e765a5aff7e694852014
Lenoch <81lennoch@gmail.com>
2024-07-06 23:44:55 +0200

    Improve secret key security if default is not changed
    Added a check to generate a new secret key if the default key is detected.

--- app/__init__.py
+++ app/__init__.py
@@ -10,11 +10,14 @@ from run import settings
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
     
-    app.secret_key = settings.get('app-secret-key', secrets.token_hex(16))    
-    app.jinja_env.add_extension('jinja2.ext.do')
-    
+    app.secret_key = settings.get('app-secret-key', secrets.token_hex(16))
+    if app.secret_key == '93edb4c7329455e5aeff7e43f837e6be':  # default key
+        app.secret_key = secrets.token_hex(16)
+        
     if not settings.get('production', True):
         os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' # oauth ssl if needed
+    
+    app.jinja_env.add_extension('jinja2.ext.do')
     
     app.register_blueprint(home_bp)
     app.register_blueprint(misc_bp)
