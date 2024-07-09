7210465c44bbd36643e3c20c7243ac9f73102d98
Lenoch <81lennoch@gmail.com>
2024-07-09 19:17:05 +0200

    Add English .lang file for `/about` page
    - Added the content of the `/about` page in the .lang file.
    - Enabled loading the `/about` page in different languages via language-specific URLs (e.g., `/<lang>/about`).

--- app/routes/about.py
+++ app/routes/about.py
@@ -1,19 +1,36 @@
 from flask import Blueprint, render_template, redirect
 from datetime import datetime
 from run import settings
+from app.utils.languages import text, language_exists
 
 about_bp = Blueprint('about', __name__)
 
 
 @about_bp.route('/about')
+@about_bp.route('/about/')
 def about():
     return render_template(
-        'about.jinja',
+        'about.jinja', lang='en',
         settings=settings['site']['index'],
         year=datetime.now().year,
-        open=open
+        open=open,
+        text=text
     )
 
+@about_bp.route('/<language>/about')
+@about_bp.route('/<language>/about/')
+def index_lang(language):
+    if not language_exists(language):
+        return redirect('/about')
+        
+    return render_template(
+        'about.jinja', lang=language,
+        settings=settings['site']['index'],
+        year=datetime.now().year,
+        open=open,
+        text=text
+    )
+    
 @about_bp.route('/about/')
 def redirect_about():
     return redirect('/about')
