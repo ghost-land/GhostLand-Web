d510edb38b50334bb6eb152e857d14ae39b708a8
Lenoch <81lennoch@gmail.com>
2024-07-12 00:54:38 +0200

    Update default home language to English

--- app/routes/home.py
+++ app/routes/home.py
@@ -9,7 +9,7 @@ home_bp = Blueprint('home', __name__)
 @home_bp.route('/')
 def index():
     return render_template(
-        'index.jinja', lang='fr',
+        'index.jinja', lang='en',
         settings=settings['site']['index'],
         year=datetime.now().year,
         open=open,
