77f63ac6e5e77237338badd0801f2e08817d35bb
Lenoch <81lennoch@gmail.com>
2024-07-12 19:07:35 +0200

    Add responsive to main page
    Updated styles and layout to ensure the index page is responsive across different devices and screen sizes.

---
+++ static/js/header.js
@@ -0,0 +1,5 @@
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
