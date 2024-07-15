abfb46e68eb80dbb6e19834e6de0ac1c11065f32
Lenoch <81lennoch@gmail.com>
2024-07-15 23:45:49 +0200

    Add language selector in the header
    Implemented a language selector in the header for all pages. Allows users to switch between available languages easily.

--- static/js/header.js
+++ static/js/header.js
@@ -3,3 +3,26 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+// Language selector functionality
+document.addEventListener('DOMContentLoaded', function() {
+    const langSelector = document.getElementById('lang-select')
+    if (langSelector) {
+        // Set default value to langSelector using URL
+        const currentPath = window.location.pathname
+        const langCode = currentPath.split('/')[1]
+        if (langCode && langCode.length == 2) {
+            langSelector.value = langCode
+        }
+
+        langSelector.addEventListener('change', function() {
+            const selectedLang = this.value
+            console.log('Selected language:' + selectedLang);
+            if (langCode && langCode.length == 2) {
+                window.location.href = '/' + selectedLang + '/' + window.location.pathname.slice(3)
+            } else {
+                window.location.href = '/' + selectedLang + '/' + window.location.pathname
+            }
+        })
+    }
+})
